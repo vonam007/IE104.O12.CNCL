9bbcf87d1d43285a046830496d271cb4437fa94e
Võ Nguyễn Hoài Nam <vonguyenhoainam123@gmail.com>
2023-12-03 14:55:39 +0700

    update some payment

--- JS/payment.js
+++ JS/payment.js
@@ -1,3 +1,17 @@
+
+let paymentButton = document.getElementById("payment-btn");
+paymentButton.addEventListener("click", showMethod);
+
+function showMethod() {
+    if (document.getElementById("payment-method").style.height == "auto") {
+        document.getElementById("payment-method").style.height = "0";
+        paymentButton.style.removeProperty("transform");
+    } else {
+        document.getElementById("payment-method").style.height = "auto";
+        paymentButton.style.transform = "rotate(90deg)";
+    }
+}
+
 const paymentSuccess = () => {
     window.location.href = "pay-success.html";
 }
