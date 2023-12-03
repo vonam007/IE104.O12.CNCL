c0956aa1771a5d6c1b5cdd23ae23f1b9aa56f5df
Võ Nguyễn Hoài Nam <vonguyenhoainam123@gmail.com>
2023-12-03 13:11:30 +0700

    link pay-success
    link page success to payment pages

--- JS/payment.js
+++ JS/payment.js
@@ -1,34 +1,3 @@
 const paymentSuccess = () => {
-    let SuccessAnnouncement = document.getElementById("payment-success");
-    SuccessAnnouncement.style.display = "flex";
-    CountDown();
-    setTimeout(paymentEnd, 11000);
-}
-const paymentEnd = () => {
-    let SuccessAnnouncement = document.getElementById("payment-success");
-    SuccessAnnouncement.style.display = "none";
-    window.location.href = "../index.html";
-}
-
-const CountDown = () => {
-    let countDownSec = document.getElementById("countdown");
-    var countDown = 11;
-    setInterval(() => {
-        countDown--;
-        countDownSec.innerHTML = "(" + countDown + "s sau tự động chuyển về trang chủ)";
-    }, 1000);
-}
-
-
-let paymentButton = document.getElementById("payment-btn");
-paymentButton.addEventListener("click", showMethod);
-
-function showMethod() {
-    if (document.getElementById("payment-method").style.display == "block") {
-        document.getElementById("payment-method").style.display = "none";
-        paymentButton.style.removeProperty("transform");
-    } else {
-        document.getElementById("payment-method").style.display = "block";
-        paymentButton.style.transform = "rotate(90deg)";
-    }
+    window.location.href = "pay-success.html";
 }
